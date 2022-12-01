aa870612c0dcacd98032ea9bd42b9945a9b162ee
Yurii Osadchyi <yurii.osadchyi@bloxroute.com>
2022-12-01 17:06:20 +0200

    TRAD-382 Add GetBlockStream support

--- js/ffi/proto/google/api/field_behavior_pb.d.ts
+++ js/ffi/proto/google/api/field_behavior_pb.d.ts
@@ -4,10 +4,10 @@
 /* tslint:disable */
 /* eslint-disable */
 
-import * as jspb from "google-protobuf";
-import * as google_protobuf_descriptor_pb from "google-protobuf/google/protobuf/descriptor_pb";
+import * as jspb from "google-protobuf"
+import * as google_protobuf_descriptor_pb from "google-protobuf/google/protobuf/descriptor_pb"
 
-export const fieldBehavior: jspb.ExtensionFieldInfo<FieldBehavior>;
+export const fieldBehavior: jspb.ExtensionFieldInfo<FieldBehavior>
 
 export enum FieldBehavior {
     FIELD_BEHAVIOR_UNSPECIFIED = 0,
