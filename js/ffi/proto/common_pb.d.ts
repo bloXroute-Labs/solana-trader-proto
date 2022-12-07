bb4507483f9ba3bff40c2674273daac65fae2203
Yurii Osadchyi <yurii.osadchyi@bloxroute.com>
2022-12-07 09:59:55 +0200

    TRAD-386 Add block stream: generate latest files

--- js/ffi/proto/common_pb.d.ts
+++ js/ffi/proto/common_pb.d.ts
@@ -4,54 +4,54 @@
 /* tslint:disable */
 /* eslint-disable */
 
-import * as jspb from "google-protobuf"
+import * as jspb from "google-protobuf";
 
-export class PriceImpactPercent extends jspb.Message {
-    getPercent(): number
-    setPercent(value: number): PriceImpactPercent
-    getInfinity(): Infinity
-    setInfinity(value: Infinity): PriceImpactPercent
+export class PriceImpactPercent extends jspb.Message { 
+    getPercent(): number;
+    setPercent(value: number): PriceImpactPercent;
+    getInfinity(): Infinity;
+    setInfinity(value: Infinity): PriceImpactPercent;
 
-    serializeBinary(): Uint8Array
-    toObject(includeInstance?: boolean): PriceImpactPercent.AsObject
-    static toObject(includeInstance: boolean, msg: PriceImpactPercent): PriceImpactPercent.AsObject
-    static extensions: { [key: number]: jspb.ExtensionFieldInfo<jspb.Message> }
-    static extensionsBinary: { [key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message> }
-    static serializeBinaryToWriter(message: PriceImpactPercent, writer: jspb.BinaryWriter): void
-    static deserializeBinary(bytes: Uint8Array): PriceImpactPercent
-    static deserializeBinaryFromReader(message: PriceImpactPercent, reader: jspb.BinaryReader): PriceImpactPercent
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): PriceImpactPercent.AsObject;
+    static toObject(includeInstance: boolean, msg: PriceImpactPercent): PriceImpactPercent.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: PriceImpactPercent, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): PriceImpactPercent;
+    static deserializeBinaryFromReader(message: PriceImpactPercent, reader: jspb.BinaryReader): PriceImpactPercent;
 }
 
 export namespace PriceImpactPercent {
     export type AsObject = {
-        percent: number
-        infinity: Infinity
+        percent: number,
+        infinity: Infinity,
     }
 }
 
-export class Fee extends jspb.Message {
-    getAmount(): number
-    setAmount(value: number): Fee
-    getMint(): string
-    setMint(value: string): Fee
-    getPercent(): number
-    setPercent(value: number): Fee
+export class Fee extends jspb.Message { 
+    getAmount(): number;
+    setAmount(value: number): Fee;
+    getMint(): string;
+    setMint(value: string): Fee;
+    getPercent(): number;
+    setPercent(value: number): Fee;
 
-    serializeBinary(): Uint8Array
-    toObject(includeInstance?: boolean): Fee.AsObject
-    static toObject(includeInstance: boolean, msg: Fee): Fee.AsObject
-    static extensions: { [key: number]: jspb.ExtensionFieldInfo<jspb.Message> }
-    static extensionsBinary: { [key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message> }
-    static serializeBinaryToWriter(message: Fee, writer: jspb.BinaryWriter): void
-    static deserializeBinary(bytes: Uint8Array): Fee
-    static deserializeBinaryFromReader(message: Fee, reader: jspb.BinaryReader): Fee
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): Fee.AsObject;
+    static toObject(includeInstance: boolean, msg: Fee): Fee.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: Fee, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): Fee;
+    static deserializeBinaryFromReader(message: Fee, reader: jspb.BinaryReader): Fee;
 }
 
 export namespace Fee {
     export type AsObject = {
-        amount: number
-        mint: string
-        percent: number
+        amount: number,
+        mint: string,
+        percent: number,
     }
 }
 
--- js/ffi/proto/google/api/http_pb.d.ts
+++ js/ffi/proto/google/api/http_pb.d.ts
@@ -4,99 +4,99 @@
 /* tslint:disable */
 /* eslint-disable */
 
-import * as jspb from "google-protobuf"
+import * as jspb from "google-protobuf";
 
-export class Http extends jspb.Message {
-    clearRulesList(): void
-    getRulesList(): Array<HttpRule>
-    setRulesList(value: Array<HttpRule>): Http
-    addRules(value?: HttpRule, index?: number): HttpRule
-    getFullyDecodeReservedExpansion(): boolean
-    setFullyDecodeReservedExpansion(value: boolean): Http
+export class Http extends jspb.Message { 
+    clearRulesList(): void;
+    getRulesList(): Array<HttpRule>;
+    setRulesList(value: Array<HttpRule>): Http;
+    addRules(value?: HttpRule, index?: number): HttpRule;
+    getFullyDecodeReservedExpansion(): boolean;
+    setFullyDecodeReservedExpansion(value: boolean): Http;
 
-    serializeBinary(): Uint8Array
-    toObject(includeInstance?: boolean): Http.AsObject
-    static toObject(includeInstance: boolean, msg: Http): Http.AsObject
-    static extensions: { [key: number]: jspb.ExtensionFieldInfo<jspb.Message> }
-    static extensionsBinary: { [key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message> }
-    static serializeBinaryToWriter(message: Http, writer: jspb.BinaryWriter): void
-    static deserializeBinary(bytes: Uint8Array): Http
-    static deserializeBinaryFromReader(message: Http, reader: jspb.BinaryReader): Http
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): Http.AsObject;
+    static toObject(includeInstance: boolean, msg: Http): Http.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: Http, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): Http;
+    static deserializeBinaryFromReader(message: Http, reader: jspb.BinaryReader): Http;
 }
 
 export namespace Http {
     export type AsObject = {
-        rulesList: Array<HttpRule.AsObject>
-        fullyDecodeReservedExpansion: boolean
+        rulesList: Array<HttpRule.AsObject>,
+        fullyDecodeReservedExpansion: boolean,
     }
 }
 
-export class HttpRule extends jspb.Message {
-    getSelector(): string
-    setSelector(value: string): HttpRule
+export class HttpRule extends jspb.Message { 
+    getSelector(): string;
+    setSelector(value: string): HttpRule;
 
-    hasGet(): boolean
-    clearGet(): void
-    getGet(): string
-    setGet(value: string): HttpRule
+    hasGet(): boolean;
+    clearGet(): void;
+    getGet(): string;
+    setGet(value: string): HttpRule;
 
-    hasPut(): boolean
-    clearPut(): void
-    getPut(): string
-    setPut(value: string): HttpRule
+    hasPut(): boolean;
+    clearPut(): void;
+    getPut(): string;
+    setPut(value: string): HttpRule;
 
-    hasPost(): boolean
-    clearPost(): void
-    getPost(): string
-    setPost(value: string): HttpRule
+    hasPost(): boolean;
+    clearPost(): void;
+    getPost(): string;
+    setPost(value: string): HttpRule;
 
-    hasDelete(): boolean
-    clearDelete(): void
-    getDelete(): string
-    setDelete(value: string): HttpRule
+    hasDelete(): boolean;
+    clearDelete(): void;
+    getDelete(): string;
+    setDelete(value: string): HttpRule;
 
-    hasPatch(): boolean
-    clearPatch(): void
-    getPatch(): string
-    setPatch(value: string): HttpRule
+    hasPatch(): boolean;
+    clearPatch(): void;
+    getPatch(): string;
+    setPatch(value: string): HttpRule;
 
-    hasCustom(): boolean
-    clearCustom(): void
-    getCustom(): CustomHttpPattern | undefined
-    setCustom(value?: CustomHttpPattern): HttpRule
-    getBody(): string
-    setBody(value: string): HttpRule
-    getResponseBody(): string
-    setResponseBody(value: string): HttpRule
-    clearAdditionalBindingsList(): void
-    getAdditionalBindingsList(): Array<HttpRule>
-    setAdditionalBindingsList(value: Array<HttpRule>): HttpRule
-    addAdditionalBindings(value?: HttpRule, index?: number): HttpRule
+    hasCustom(): boolean;
+    clearCustom(): void;
+    getCustom(): CustomHttpPattern | undefined;
+    setCustom(value?: CustomHttpPattern): HttpRule;
+    getBody(): string;
+    setBody(value: string): HttpRule;
+    getResponseBody(): string;
+    setResponseBody(value: string): HttpRule;
+    clearAdditionalBindingsList(): void;
+    getAdditionalBindingsList(): Array<HttpRule>;
+    setAdditionalBindingsList(value: Array<HttpRule>): HttpRule;
+    addAdditionalBindings(value?: HttpRule, index?: number): HttpRule;
 
-    getPatternCase(): HttpRule.PatternCase
+    getPatternCase(): HttpRule.PatternCase;
 
-    serializeBinary(): Uint8Array
-    toObject(includeInstance?: boolean): HttpRule.AsObject
-    static toObject(includeInstance: boolean, msg: HttpRule): HttpRule.AsObject
-    static extensions: { [key: number]: jspb.ExtensionFieldInfo<jspb.Message> }
-    static extensionsBinary: { [key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message> }
-    static serializeBinaryToWriter(message: HttpRule, writer: jspb.BinaryWriter): void
-    static deserializeBinary(bytes: Uint8Array): HttpRule
-    static deserializeBinaryFromReader(message: HttpRule, reader: jspb.BinaryReader): HttpRule
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): HttpRule.AsObject;
+    static toObject(includeInstance: boolean, msg: HttpRule): HttpRule.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: HttpRule, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): HttpRule;
+    static deserializeBinaryFromReader(message: HttpRule, reader: jspb.BinaryReader): HttpRule;
 }
 
 export namespace HttpRule {
     export type AsObject = {
-        selector: string
-        get: string
-        put: string
-        post: string
-        pb_delete: string
-        patch: string
-        custom?: CustomHttpPattern.AsObject
-        body: string
-        responseBody: string
-        additionalBindingsList: Array<HttpRule.AsObject>
+        selector: string,
+        get: string,
+        put: string,
+        post: string,
+        pb_delete: string,
+        patch: string,
+        custom?: CustomHttpPattern.AsObject,
+        body: string,
+        responseBody: string,
+        additionalBindingsList: Array<HttpRule.AsObject>,
     }
 
     export enum PatternCase {
@@ -108,27 +108,28 @@ export namespace HttpRule {
         PATCH = 6,
         CUSTOM = 8,
     }
+
 }
 
-export class CustomHttpPattern extends jspb.Message {
-    getKind(): string
-    setKind(value: string): CustomHttpPattern
-    getPath(): string
-    setPath(value: string): CustomHttpPattern
+export class CustomHttpPattern extends jspb.Message { 
+    getKind(): string;
+    setKind(value: string): CustomHttpPattern;
+    getPath(): string;
+    setPath(value: string): CustomHttpPattern;
 
-    serializeBinary(): Uint8Array
-    toObject(includeInstance?: boolean): CustomHttpPattern.AsObject
-    static toObject(includeInstance: boolean, msg: CustomHttpPattern): CustomHttpPattern.AsObject
-    static extensions: { [key: number]: jspb.ExtensionFieldInfo<jspb.Message> }
-    static extensionsBinary: { [key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message> }
-    static serializeBinaryToWriter(message: CustomHttpPattern, writer: jspb.BinaryWriter): void
-    static deserializeBinary(bytes: Uint8Array): CustomHttpPattern
-    static deserializeBinaryFromReader(message: CustomHttpPattern, reader: jspb.BinaryReader): CustomHttpPattern
+    serializeBinary(): Uint8Array;
+    toObject(includeInstance?: boolean): CustomHttpPattern.AsObject;
+    static toObject(includeInstance: boolean, msg: CustomHttpPattern): CustomHttpPattern.AsObject;
+    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+    static serializeBinaryToWriter(message: CustomHttpPattern, writer: jspb.BinaryWriter): void;
+    static deserializeBinary(bytes: Uint8Array): CustomHttpPattern;
+    static deserializeBinaryFromReader(message: CustomHttpPattern, reader: jspb.BinaryReader): CustomHttpPattern;
 }
 
 export namespace CustomHttpPattern {
     export type AsObject = {
-        kind: string
-        path: string
+        kind: string,
+        path: string,
     }
 }
