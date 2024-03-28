60ac242a41e1be9fce415c640e3d469b83239a4b
Mahmoud Taabodi <mahmoud.taabodi@bloxroute.com>
2024-03-28 13:42:24 -0400

    Adding meta for unknown pools

--- python/src/bxsolana_trader_proto/api/__init__.py
+++ python/src/bxsolana_trader_proto/api/__init__.py
@@ -1017,16 +1017,16 @@ class TransactionMeta(betterproto.Message):
     fee: int = betterproto.uint64_field(3)
     pre_balances: List[int] = betterproto.uint64_field(4)
     post_balances: List[int] = betterproto.uint64_field(5)
-    inner_instructions: List["TransactionMetaInnerInstruction"] = (
-        betterproto.message_field(6)
-    )
+    inner_instructions: List[
+        "TransactionMetaInnerInstruction"
+    ] = betterproto.message_field(6)
     log_messages: List[str] = betterproto.string_field(7)
     pre_token_balances: List["TransactionMetaTokenBalance"] = betterproto.message_field(
         8
     )
-    post_token_balances: List["TransactionMetaTokenBalance"] = (
-        betterproto.message_field(9)
-    )
+    post_token_balances: List[
+        "TransactionMetaTokenBalance"
+    ] = betterproto.message_field(9)
 
 
 @dataclass(eq=False, repr=False)
@@ -1106,6 +1106,13 @@ class GetSwapsStreamResponse(betterproto.Message):
     slot: int = betterproto.int64_field(1)
     swap: "GetSwapsStreamUpdate" = betterproto.message_field(2)
     timestamp: datetime = betterproto.message_field(3)
+    meta: "GetSwapsStreamUpdateMeta" = betterproto.message_field(4)
+
+
+@dataclass(eq=False, repr=False)
+class GetSwapsStreamUpdateMeta(betterproto.Message):
+    message: str = betterproto.string_field(1)
+    unknown_pools: List[str] = betterproto.string_field(2)
 
 
 @dataclass(eq=False, repr=False)
@@ -1212,6 +1219,13 @@ class GetPoolReservesStreamResponse(betterproto.Message):
     slot: int = betterproto.int64_field(1)
     reserves: "PoolReserves" = betterproto.message_field(2)
     timestamp: datetime = betterproto.message_field(3)
+    meta: "GetPoolReservesStreamResponseMeta" = betterproto.message_field(4)
+
+
+@dataclass(eq=False, repr=False)
+class GetPoolReservesStreamResponseMeta(betterproto.Message):
+    message: str = betterproto.string_field(1)
+    unknown_pools: List[str] = betterproto.string_field(2)
 
 
 @dataclass(eq=False, repr=False)
