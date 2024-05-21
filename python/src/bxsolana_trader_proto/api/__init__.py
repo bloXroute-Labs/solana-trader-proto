1e836cb94d13aa460b86f2c05b9aaed261199d41
Dumitru Cantemir <101207299+DimaCantemir@users.noreply.github.com>
2024-05-21 12:05:41 -0500

    removed on demand tpu field (#180)
    removed on demand tpu field

--- python/src/bxsolana_trader_proto/api/__init__.py
+++ python/src/bxsolana_trader_proto/api/__init__.py
@@ -463,7 +463,6 @@ class PostSubmitRequest(betterproto.Message):
         3, optional=True, group="_frontRunningProtection"
     )
     tip: Optional[int] = betterproto.uint64_field(4, optional=True, group="_tip")
-    tpu: Optional[int] = betterproto.uint32_field(5, optional=True, group="_tpu")
     use_staked_rp_cs: Optional[bool] = betterproto.bool_field(
         6, optional=True, group="_useStakedRPCs"
     )
