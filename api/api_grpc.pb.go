81329e22478998b3484ffe073f86c0c12132a487
Mahmoud Taabodi <mahmoud.taabodi@bloxroute.com>
2024-08-28 13:39:11 -0400

    Rebasing latest develop

--- api/api_grpc.pb.go
+++ api/api_grpc.pb.go
@@ -97,6 +97,7 @@ type ApiClient interface {
 	GetPricesStream(ctx context.Context, in *GetPricesStreamRequest, opts ...grpc.CallOption) (Api_GetPricesStreamClient, error)
 	GetNewRaydiumPoolsStream(ctx context.Context, in *GetNewRaydiumPoolsRequest, opts ...grpc.CallOption) (Api_GetNewRaydiumPoolsStreamClient, error)
 	GetSwapsStream(ctx context.Context, in *GetSwapsStreamRequest, opts ...grpc.CallOption) (Api_GetSwapsStreamClient, error)
+	GetPumpFunSwapsStream(ctx context.Context, in *GetPumpFunSwapsStreamRequest, opts ...grpc.CallOption) (Api_GetPumpFunSwapsStreamClient, error)
 }
 
 type apiClient struct {
@@ -1109,6 +1110,38 @@ func (x *apiGetSwapsStreamClient) Recv() (*GetSwapsStreamResponse, error) {
 	return m, nil
 }
 
+func (c *apiClient) GetPumpFunSwapsStream(ctx context.Context, in *GetPumpFunSwapsStreamRequest, opts ...grpc.CallOption) (Api_GetPumpFunSwapsStreamClient, error) {
+	stream, err := c.cc.NewStream(ctx, &Api_ServiceDesc.Streams[15], "/api.Api/GetPumpFunSwapsStream", opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &apiGetPumpFunSwapsStreamClient{stream}
+	if err := x.ClientStream.SendMsg(in); err != nil {
+		return nil, err
+	}
+	if err := x.ClientStream.CloseSend(); err != nil {
+		return nil, err
+	}
+	return x, nil
+}
+
+type Api_GetPumpFunSwapsStreamClient interface {
+	Recv() (*GetPumpFunSwapsStreamResponse, error)
+	grpc.ClientStream
+}
+
+type apiGetPumpFunSwapsStreamClient struct {
+	grpc.ClientStream
+}
+
+func (x *apiGetPumpFunSwapsStreamClient) Recv() (*GetPumpFunSwapsStreamResponse, error) {
+	m := new(GetPumpFunSwapsStreamResponse)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
 // ApiServer is the server API for Api service.
 // All implementations must embed UnimplementedApiServer
 // for forward compatibility
@@ -1192,6 +1225,7 @@ type ApiServer interface {
 	GetPricesStream(*GetPricesStreamRequest, Api_GetPricesStreamServer) error
 	GetNewRaydiumPoolsStream(*GetNewRaydiumPoolsRequest, Api_GetNewRaydiumPoolsStreamServer) error
 	GetSwapsStream(*GetSwapsStreamRequest, Api_GetSwapsStreamServer) error
+	GetPumpFunSwapsStream(*GetPumpFunSwapsStreamRequest, Api_GetPumpFunSwapsStreamServer) error
 	mustEmbedUnimplementedApiServer()
 }
 
@@ -1417,6 +1451,9 @@ func (UnimplementedApiServer) GetNewRaydiumPoolsStream(*GetNewRaydiumPoolsReques
 }
 func (UnimplementedApiServer) GetSwapsStream(*GetSwapsStreamRequest, Api_GetSwapsStreamServer) error {
 	return status.Errorf(codes.Unimplemented, "method GetSwapsStream not implemented")
+}
+func (UnimplementedApiServer) GetPumpFunSwapsStream(*GetPumpFunSwapsStreamRequest, Api_GetPumpFunSwapsStreamServer) error {
+	return status.Errorf(codes.Unimplemented, "method GetPumpFunSwapsStream not implemented")
 }
 func (UnimplementedApiServer) mustEmbedUnimplementedApiServer() {}
 
@@ -2790,6 +2827,27 @@ func (x *apiGetSwapsStreamServer) Send(m *GetSwapsStreamResponse) error {
 	return x.ServerStream.SendMsg(m)
 }
 
+func _Api_GetPumpFunSwapsStream_Handler(srv interface{}, stream grpc.ServerStream) error {
+	m := new(GetPumpFunSwapsStreamRequest)
+	if err := stream.RecvMsg(m); err != nil {
+		return err
+	}
+	return srv.(ApiServer).GetPumpFunSwapsStream(m, &apiGetPumpFunSwapsStreamServer{stream})
+}
+
+type Api_GetPumpFunSwapsStreamServer interface {
+	Send(*GetPumpFunSwapsStreamResponse) error
+	grpc.ServerStream
+}
+
+type apiGetPumpFunSwapsStreamServer struct {
+	grpc.ServerStream
+}
+
+func (x *apiGetPumpFunSwapsStreamServer) Send(m *GetPumpFunSwapsStreamResponse) error {
+	return x.ServerStream.SendMsg(m)
+}
+
 // Api_ServiceDesc is the grpc.ServiceDesc for Api service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -3104,6 +3162,11 @@ var Api_ServiceDesc = grpc.ServiceDesc{
 		{
 			StreamName:    "GetSwapsStream",
 			Handler:       _Api_GetSwapsStream_Handler,
+			ServerStreams: true,
+		},
+		{
+			StreamName:    "GetPumpFunSwapsStream",
+			Handler:       _Api_GetPumpFunSwapsStream_Handler,
 			ServerStreams: true,
 		},
 	},
--- python/src/bxsolana_trader_proto/api/__init__.py
+++ python/src/bxsolana_trader_proto/api/__init__.py
@@ -1554,6 +1554,27 @@ class OrderV2(betterproto.Message):
     open_order_account: str = betterproto.string_field(9)
 
 
+@dataclass(eq=False, repr=False)
+class GetPumpFunSwapsStreamRequest(betterproto.Message):
+    tokens: List[str] = betterproto.string_field(1)
+
+
+@dataclass(eq=False, repr=False)
+class GetPumpFunSwapsStreamResponse(betterproto.Message):
+    slot: int = betterproto.int64_field(1)
+    txn_hash: str = betterproto.string_field(2)
+    mint_address: str = betterproto.string_field(3)
+    user_token_account_address: str = betterproto.string_field(4)
+    bonding_curve_address: str = betterproto.string_field(5)
+    token_vault_address: str = betterproto.string_field(6)
+    sol_amount: int = betterproto.uint64_field(7)
+    token_amount: int = betterproto.uint64_field(8)
+    is_buy: bool = betterproto.bool_field(9)
+    virtual_sol_reserves: int = betterproto.uint64_field(10)
+    virtual_token_reserves: int = betterproto.uint64_field(11)
+    timestamp: datetime = betterproto.message_field(12)
+
+
 class ApiStub(betterproto.ServiceStub):
     async def get_rate_limit(
         self,
@@ -2811,6 +2832,24 @@ class ApiStub(betterproto.ServiceStub):
         ):
             yield response
 
+    async def get_pump_fun_swaps_stream(
+        self,
+        get_pump_fun_swaps_stream_request: "GetPumpFunSwapsStreamRequest",
+        *,
+        timeout: Optional[float] = None,
+        deadline: Optional["Deadline"] = None,
+        metadata: Optional["MetadataLike"] = None
+    ) -> AsyncIterator["GetPumpFunSwapsStreamResponse"]:
+        async for response in self._unary_stream(
+            "/api.Api/GetPumpFunSwapsStream",
+            get_pump_fun_swaps_stream_request,
+            GetPumpFunSwapsStreamResponse,
+            timeout=timeout,
+            deadline=deadline,
+            metadata=metadata,
+        ):
+            yield response
+
 
 class ApiBase(ServiceBase):
     async def get_rate_limit(
@@ -3195,6 +3234,12 @@ class ApiBase(ServiceBase):
     ) -> AsyncIterator["GetSwapsStreamResponse"]:
         raise grpclib.GRPCError(grpclib.const.Status.UNIMPLEMENTED)
         yield GetSwapsStreamResponse()
+
+    async def get_pump_fun_swaps_stream(
+        self, get_pump_fun_swaps_stream_request: "GetPumpFunSwapsStreamRequest"
+    ) -> AsyncIterator["GetPumpFunSwapsStreamResponse"]:
+        raise grpclib.GRPCError(grpclib.const.Status.UNIMPLEMENTED)
+        yield GetPumpFunSwapsStreamResponse()
 
     async def __rpc_get_rate_limit(
         self, stream: "grpclib.server.Stream[GetRateLimitRequest, GetRateLimitResponse]"
@@ -3799,6 +3844,17 @@ class ApiBase(ServiceBase):
             request,
         )
 
+    async def __rpc_get_pump_fun_swaps_stream(
+        self,
+        stream: "grpclib.server.Stream[GetPumpFunSwapsStreamRequest, GetPumpFunSwapsStreamResponse]",
+    ) -> None:
+        request = await stream.recv_message()
+        await self._call_rpc_handler_server_stream(
+            self.get_pump_fun_swaps_stream,
+            stream,
+            request,
+        )
+
     def __mapping__(self) -> Dict[str, grpclib.const.Handler]:
         return {
             "/api.Api/GetRateLimit": grpclib.const.Handler(
@@ -4238,5 +4294,11 @@ class ApiBase(ServiceBase):
                 grpclib.const.Cardinality.UNARY_STREAM,
                 GetSwapsStreamRequest,
                 GetSwapsStreamResponse,
+            ),
+            "/api.Api/GetPumpFunSwapsStream": grpclib.const.Handler(
+                self.__rpc_get_pump_fun_swaps_stream,
+                grpclib.const.Cardinality.UNARY_STREAM,
+                GetPumpFunSwapsStreamRequest,
+                GetPumpFunSwapsStreamResponse,
             ),
         }
