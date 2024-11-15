9049527b44e53fe29aa65a82dd47491bccbac091
Lanikean <jose.bravo@bloxroute.com>
2024-11-15 12:30:53 -0700

    fix fee & block time fields

--- build.rs
+++ build.rs
@@ -187,9 +187,12 @@ fn add_field_attributes(builder: Builder) -> Builder {
             "#[serde(deserialize_with = \"string_to_u64\")]",
         )
         .field_attribute(
-            "blockTime",
+            "block_time",
             "#[serde(deserialize_with = \"string_to_u64\")]",
         )
-
+        .field_attribute(
+            "api.TransactionMeta.fee",
+            "#[serde(deserialize_with = \"string_to_u64\")]",
+        )
         .field_attribute("data", "#[serde(deserialize_with = \"string_to_bytes\")]")
 }
