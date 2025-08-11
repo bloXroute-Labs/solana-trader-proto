65af53a1257a9ba128272e1dbf6176c429023632
dumitrucantemir <dumitru.cantemir@bloxroute.com>
2025-08-11 18:42:57 -0500

    Added support for SlotInfo endpoint

--- api/api.pb.gw.go
+++ api/api.pb.gw.go
@@ -4036,7 +4036,7 @@ func RegisterApiHandlerServer(ctx context.Context, mux *runtime.ServeMux, server
 		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
 		var err error
-		ctx, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/api.Api/GetServerTime", runtime.WithHTTPPathPattern("/api/v1/system/time"))
+		ctx, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/api.Api/GetServerTime", runtime.WithHTTPPathPattern("/api/v2/system/time"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
@@ -5692,7 +5692,7 @@ func RegisterApiHandlerClient(ctx context.Context, mux *runtime.ServeMux, client
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
 		var err error
-		ctx, err = runtime.AnnotateContext(ctx, mux, req, "/api.Api/GetServerTime", runtime.WithHTTPPathPattern("/api/v1/system/time"))
+		ctx, err = runtime.AnnotateContext(ctx, mux, req, "/api.Api/GetServerTime", runtime.WithHTTPPathPattern("/api/v2/system/time"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
@@ -6369,7 +6369,7 @@ var (
 
 	pattern_Api_GetQuotes_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "v1", "market", "quote"}, ""))
 
-	pattern_Api_GetServerTime_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "v1", "system", "time"}, ""))
+	pattern_Api_GetServerTime_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "v2", "system", "time"}, ""))
 
 	pattern_Api_GetSlotInfo_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "v1", "system", "slot-info"}, ""))
 
