d69d34f7f5a4d18fdf8b4cf2a58dbbe47296172a
dumitrucantemir <dumitru.cantemir@bloxroute.com>
2025-09-04 14:41:27 -0500

    Added revert protection flag in PostSubmitRequest

--- python/src/bxsolana_trader_proto/api/__init__.py
+++ python/src/bxsolana_trader_proto/api/__init__.py
@@ -525,6 +525,9 @@ class PostSubmitRequest(betterproto.Message):
     submit_protection: Optional["SubmitProtection"] = betterproto.enum_field(
         12, optional=True, group="_submitProtection"
     )
+    revert_protection: Optional[bool] = betterproto.bool_field(
+        13, optional=True, group="_revertProtection"
+    )
 
 
 @dataclass(eq=False, repr=False)
@@ -1251,16 +1254,16 @@ class TransactionMeta(betterproto.Message):
     fee: int = betterproto.uint64_field(3)
     pre_balances: List[int] = betterproto.uint64_field(4)
     post_balances: List[int] = betterproto.uint64_field(5)
-    inner_instructions: List["TransactionMetaInnerInstruction"] = (
-        betterproto.message_field(6)
-    )
+    inner_instructions: List[
+        "TransactionMetaInnerInstruction"
+    ] = betterproto.message_field(6)
     log_messages: List[str] = betterproto.string_field(7)
     pre_token_balances: List["TransactionMetaTokenBalance"] = betterproto.message_field(
         8
     )
-    post_token_balances: List["TransactionMetaTokenBalance"] = (
-        betterproto.message_field(9)
-    )
+    post_token_balances: List[
+        "TransactionMetaTokenBalance"
+    ] = betterproto.message_field(9)
 
 
 @dataclass(eq=False, repr=False)
@@ -3602,7 +3605,6 @@ class ApiStub(betterproto.ServiceStub):
 
 
 class ApiBase(ServiceBase):
-
     async def get_rate_limit(
         self, get_rate_limit_request: "GetRateLimitRequest"
     ) -> "GetRateLimitResponse":
